f15b6397c22f48da1c077ce96646205c55348150
Nicholas Yang <nick@nicholasyang.com>
2022-08-27 01:30:30 -0400

    Separating out into components

---
+++ site/app/components/RepositoryActionGrid.tsx
@@ -0,0 +1,7 @@
+export default function RepositoryActionGrid() {
+  return <div className="grow grid grid-template-rows-3">
+    <div className="w-60 h-60 rounded shadow flex items-center justify-center text-xl">
+      <span className="font-bold">i</span>ssues
+    </div>
+  </div>
+}
--- site/app/routes/$owner/$name.tsx
+++ site/app/routes/$owner/$name.tsx
@@ -1,6 +1,8 @@
 import { gql } from "@apollo/client";
 import { useLoaderData } from "@remix-run/react";
 import client from "~/apollo-client";
+import { useCallback, useEffect, useState } from "react";
+import RepositoryActionGrid from "~/components/RepositoryActionGrid";
 
 export async function loader({ params }) {
   const { data } = await client.query({
@@ -21,10 +23,51 @@ export async function loader({ params }) {
   return { owner: params.owner, name: params.name, issues: data.repository.issues.nodes }
 }
 
+enum PageState {
+  Home,
+  Issues
+}
+
 export default function Repository() {
+  const [pageState, setPageState] = useState(PageState.Home);
   const { owner, name, issues } = useLoaderData();
-  return <main className="grid place-items-center">
-    <h1>{owner} / {name}</h1>
-    <ul>{issues.map(issue => <li>{issue.title}</li>)}</ul>
+
+  const handleKeyPress = useCallback((event) => {
+    switch (event.keyCode) {
+      case 73: { // i
+        setPageState(PageState.Issues);
+        break;
+      }
+      case 72: { // h
+        setPageState(PageState.Home);
+        break;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    // attach the event listener
+    document.addEventListener('keydown', handleKeyPress);
+
+    // remove the event listener
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [])
+
+  let child;
+  switch (pageState) {
+    case PageState.Home: {
+      child = <RepositoryActionGrid />
+      break;
+    }
+    case PageState.Issues: {
+      child = issues.map(issue => <ul><li>{issue.title}</li></ul>)
+    }
+  }
+
+  return <main className="flex flex-col items-center justify-between h-screen">
+    <div className="grow flex items-center"><h1><span className="font-bold">{owner}</span> / {name}</h1></div>
+    {child}
   </main>
 }
